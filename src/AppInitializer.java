245970229057f05ca301262641b071b5d1b7f4be
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-15 17:33:48 +0530

    updated!

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AppInitializer {
     public static void main(String[] args) {
-
+        try {
+            //Driver load
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //create a connection
+            Connection con = DriverManager.getConnection('','','')
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
